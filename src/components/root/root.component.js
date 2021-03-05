1362f9b05379290fd1daea5b57fac2f5270514ae
Mikhail Tkachenko <m.tkachenko@aimprosoft.com>
2021-03-05 21:14:22 +0200

    remove redundant components revert

--- src/components/root/root.component.js
+++ src/components/root/root.component.js
@@ -8,16 +8,30 @@ export class RootComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            headerText: 'column1',
-            headerImgSrc: 'https://391795-1232725-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2016/08/Bear-Worksheets.jpg',
-            searchValue: ''
+            columns: [
+                {
+                    headerText: 'column1',
+                    headerImgSrc: 'https://391795-1232725-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2016/08/Bear-Worksheets.jpg',
+                    searchValue: ''
+                },
+                {
+                    headerText: 'column2',
+                    headerImgSrc: 'https://391795-1232725-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2016/08/Bear-Worksheets.jpg',
+                    searchValue: '',
+                },
+            ]
         }
+
+
     }
 
-    onSearchValueChange(newValue) {
-        this.setState({
-            searchValue: newValue
-        });
+    onSearchValueChange(columnIndex, newValue) {
+        const newState = Object.assign({}, this.state);
+        const column = newState.columns[columnIndex];
+        column.searchValue = newValue;
+
+        // clone state
+        this.setState(newState);
     }
 
     render() {
@@ -27,14 +41,20 @@ export class RootComponent extends React.Component {
                     <thead>
                         <tr>
                             <th>
-                                <ColumnHeaderComponent text={this.state.headerText} imgSrc={this.state.headerImgSrc}/>
+                                <ColumnHeaderComponent text={this.state.columns[0].headerText} imgSrc={this.state.columns[0].headerImgSrc}/>
+                            </th>
+                            <th>
+                                <ColumnHeaderComponent text={this.state.columns[1].headerText} imgSrc={this.state.columns[1].headerImgSrc}/>
                             </th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                             <td>
-                                <SearchInputComponent onSerachInputValueChange={(newValue) => this.onSearchValueChange(newValue)}/>
+                                <SearchInputComponent onSerachInputValueChange={(newValue) => this.onSearchValueChange(0, newValue)}/>
+                            </td>
+                            <td>
+                                <SearchInputComponent onSerachInputValueChange={(newValue) => this.onSearchValueChange(1, newValue)}/>
                             </td>
                         </tr>
                     </tbody>
